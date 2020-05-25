54a77c3e74fc47d4b8c6e522e1c701835cb4bc41
Alexis Sellier <self@cloudhead.io>
2020-05-25 21:37:30 +0200

    Start implementing chain model

---
+++ nakamoto-chain/src/blocktree.rs
@@ -0,0 +1,133 @@
+use std::collections::HashMap;
+use std::ops::Deref;
+
+use bitcoin::blockdata::block::BlockHeader;
+use bitcoin::hash_types::BlockHash;
+use bitcoin::util::hash::BitcoinHash;
+
+use thiserror::Error;
+
+/// An error related to the block tree.
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("invalid chain")]
+    InvalidChain,
+}
+
+/// A valid hash-linked sequence of blocks.
+#[derive(Debug)]
+pub struct Blockchain(Vec<(BlockHash, BlockHeader)>);
+
+impl Blockchain {
+    pub fn try_from(blks: Vec<BlockHeader>) -> Result<Self, Error> {
+        // TODO: Validate chain.
+        let chain = blks.into_iter().map(|h| (h.bitcoin_hash(), h)).collect();
+        Ok(Self(chain))
+    }
+}
+
+impl From<Blockchain> for Vec<(BlockHash, BlockHeader)> {
+    fn from(Blockchain(blks): Blockchain) -> Self {
+        blks
+    }
+}
+
+impl Deref for Blockchain {
+    type Target = Vec<(BlockHash, BlockHeader)>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/// A representation of all known blocks that keeps track of the longest chain.
+pub trait BlockTree {
+    fn insert_block(&mut self, hash: BlockHash, block: BlockHeader) -> Option<BlockHeader>;
+    fn insert_chain(&mut self, chain: Blockchain);
+
+    fn get_block(&self, hash: &BlockHash) -> Option<&BlockHeader>;
+
+    fn chain(&self) -> ChainIter;
+    fn height(&self) -> u64;
+    fn tip(&self) -> &BlockHash;
+}
+
+/// An implementation of `BlockTree`.
+#[derive(Debug)]
+pub struct BlockCache {
+    headers: HashMap<BlockHash, BlockHeader>,
+    tip: BlockHash,
+    height: u64,
+}
+
+impl BlockCache {
+    /// Create a new `BlockCache` given the hash of the genesis block.
+    pub fn new(genesis: BlockHash) -> Self {
+        Self {
+            headers: HashMap::new(),
+            tip: genesis,
+            height: 0,
+        }
+    }
+}
+
+impl BlockTree for BlockCache {
+    fn insert_block(&mut self, hash: BlockHash, block: BlockHeader) -> Option<BlockHeader> {
+        if block.prev_blockhash == self.tip {
+            self.height += 1;
+            self.tip = hash;
+            self.headers.insert(hash, block)
+        } else {
+            None
+        }
+    }
+
+    fn insert_chain(&mut self, chain: Blockchain) {
+        Vec::from(chain).into_iter().for_each(|(hash, header)| {
+            self.insert_block(hash, header);
+        })
+    }
+
+    /// Get a block by its hash.
+    fn get_block(&self, hash: &BlockHash) -> Option<&BlockHeader> {
+        self.headers.get(hash)
+    }
+
+    /// Iterate over the longest chain, starting from the tip.
+    fn chain(&self) -> ChainIter {
+        ChainIter {
+            next: self.tip,
+            headers: &self.headers,
+        }
+    }
+
+    /// Return the height of the longest chain.
+    fn height(&self) -> u64 {
+        self.height
+    }
+
+    /// Return the tip of the longest chain.
+    fn tip(&self) -> &BlockHash {
+        &self.tip
+    }
+}
+
+/// An iterator over `BlockHash` values, starting from the tip of a chain.
+pub struct ChainIter<'a> {
+    next: BlockHash,
+    headers: &'a HashMap<BlockHash, BlockHeader>,
+}
+
+impl<'a> Iterator for ChainIter<'a> {
+    type Item = &'a BlockHeader;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.headers.get(&self.next) {
+            Some(header) => {
+                self.next = header.prev_blockhash;
+                Some(header)
+            }
+            None => None,
+        }
+    }
+}
