74814eb68465a18a481fcbb05812de6bde60d28c
Alexis Sellier <self@cloudhead.io>
2020-06-10 20:37:02 +0200

    Disable color logging output if not a tty

--- nakamoto-daemon/src/main.rs
+++ nakamoto-daemon/src/main.rs
@@ -13,22 +13,36 @@ use log;
 fn main() {
     let opts = Options::from_env();
 
-    #[cfg(feature = "fern")]
+    #[cfg(feature = "logging")]
     {
+        use atty::Stream;
         use fern::colors::{Color, ColoredLevelConfig};
 
         let colors = ColoredLevelConfig::new().info(Color::Green);
+        let stream = Stream::Stderr;
+        let io = std::io::stderr();
+        let isatty = atty::is(stream);
+
         fern::Dispatch::new()
             .format(move |out, message, record| {
-                out.finish(format_args!(
-                    "{:5} [{}] {}",
-                    colors.color(record.level()),
-                    record.target(),
-                    message
-                ))
+                if isatty {
+                    out.finish(format_args!(
+                        "{:5} [{}] {}",
+                        colors.color(record.level()),
+                        record.target(),
+                        message
+                    ))
+                } else {
+                    out.finish(format_args!(
+                        "{:5} [{}] {}",
+                        record.level(),
+                        record.target(),
+                        message
+                    ))
+                }
             })
             .level(opts.log)
-            .chain(std::io::stderr())
+            .chain(io)
             .apply()
             .unwrap();
     }
