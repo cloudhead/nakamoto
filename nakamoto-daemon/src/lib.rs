676a56b191e23561f1da303b6d3e3a00ca7a809a
Alexis Sellier <self@cloudhead.io>
2020-06-14 22:35:35 +0200

    Move daemon code into library

--- nakamoto-daemon/src/lib.rs
+++ nakamoto-daemon/src/lib.rs
@@ -1,6 +1,29 @@
+use std::io;
 use std::net;
+use std::path::Path;
+use std::sync::{Arc, RwLock};
 
 use argh::FromArgs;
+
+use nakamoto_chain as chain;
+use nakamoto_chain::block::cache::BlockCache;
+use nakamoto_chain::block::store;
+use nakamoto_p2p as p2p;
+use nakamoto_p2p::address_book::AddressBook;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    P2p(#[from] p2p::error::Error),
+    #[error(transparent)]
+    Chain(#[from] chain::block::tree::Error),
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error("Error loading address book: {0}")]
+    AddressBook(io::Error),
+}
 
 #[derive(FromArgs)]
 /// A Bitcoin light client.
@@ -22,4 +45,54 @@ impl Options {
     pub fn from_env() -> Self {
         argh::from_env()
     }
+}
+
+pub fn run(opts: Options) -> Result<(), Error> {
+    log::info!("Initializing daemon..");
+
+    let cfg = p2p::peer::Config::default();
+    let genesis = cfg.network.genesis();
+    let params = cfg.network.params();
+
+    log::info!("Genesis block hash is {}", cfg.network.genesis_hash());
+
+    let path = Path::new("headers.db");
+    let store = match store::File::create(path, genesis) {
+        Err(store::Error::Io(e)) if e.kind() == io::ErrorKind::AlreadyExists => {
+            log::info!("Found existing store {:?}", path);
+            store::File::open(path)?
+        }
+        Err(err) => panic!(err.to_string()),
+        Ok(store) => {
+            log::info!("Initializing new block store {:?}", path);
+            store
+        }
+    };
+    log::info!("Loading blocks from store..");
+
+    let cache = BlockCache::from(store, params)?;
+    let block_cache = Arc::new(RwLock::new(cache));
+    let mut net = p2p::Network::new(cfg, block_cache);
+
+    let peers = if opts.connect.is_empty() {
+        match AddressBook::load("peers") {
+            Ok(peers) if peers.is_empty() => {
+                log::info!("Address book is empty. Trying DNS seeds..");
+                AddressBook::bootstrap(cfg.network)?
+            }
+            Ok(peers) => peers,
+            Err(err) => {
+                return Err(Error::AddressBook(err));
+            }
+        }
+    } else {
+        AddressBook::from(opts.connect.as_slice())?
+    };
+
+    log::info!("{} peer(s) found..", peers.len());
+    log::debug!("{:?}", peers);
+
+    net.connect(peers)?;
+
+    Ok(())
 }
--- nakamoto-daemon/src/main.rs
+++ nakamoto-daemon/src/main.rs
@@ -1,15 +1,4 @@
-use nakamoto_chain::block::cache::BlockCache;
-use nakamoto_chain::block::store;
 use nakamoto_daemon::Options;
-use nakamoto_p2p as p2p;
-use nakamoto_p2p::address_book::AddressBook;
-
-use std::io;
-use std::path::Path;
-use std::process;
-use std::sync::{Arc, RwLock};
-
-use log;
 
 fn main() {
     let opts = Options::from_env();
@@ -48,50 +37,8 @@ fn main() {
             .unwrap();
     }
 
-    log::info!("Initializing daemon..");
-
-    let cfg = p2p::peer::Config::default();
-    let genesis = cfg.network.genesis();
-    let params = cfg.network.params();
-
-    log::info!("Genesis block hash is {}", cfg.network.genesis_hash());
-
-    let path = Path::new("headers.db");
-    let store = match store::File::create(path, genesis) {
-        Err(store::Error::Io(e)) if e.kind() == io::ErrorKind::AlreadyExists => {
-            log::info!("Found existing store {:?}", path);
-            store::File::open(path).unwrap()
-        }
-        Err(err) => panic!(err.to_string()),
-        Ok(store) => {
-            log::info!("Initializing new block store {:?}", path);
-            store
-        }
-    };
-    log::info!("Loading blocks from store..");
-
-    let cache = BlockCache::from(store, params).unwrap();
-    let block_cache = Arc::new(RwLock::new(cache));
-    let mut net = p2p::Network::new(cfg, block_cache);
-
-    let peers = if opts.connect.is_empty() {
-        match AddressBook::load("peers") {
-            Ok(peers) if peers.is_empty() => {
-                log::info!("Address book is empty. Trying DNS seeds..");
-                AddressBook::bootstrap(cfg.network).unwrap()
-            }
-            Ok(peers) => peers,
-            Err(err) => {
-                log::error!("Error loading address book: {}", err);
-                process::exit(1);
-            }
-        }
-    } else {
-        AddressBook::from(opts.connect.as_slice()).unwrap()
-    };
-
-    log::info!("{} peer(s) found..", peers.len());
-    log::debug!("{:?}", peers);
-
-    net.connect(peers).unwrap();
+    if let Err(err) = nakamoto_daemon::run(opts) {
+        log::error!("{}", err);
+        std::process::exit(1);
+    }
 }
