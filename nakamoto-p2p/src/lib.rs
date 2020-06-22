1513ce81af24694291bdcea23d33953a167bc701
Alexis Sellier <self@cloudhead.io>
2020-06-22 21:29:53 +0200

    Simplify and test network adjusted time

--- nakamoto-p2p/src/lib.rs
+++ nakamoto-p2p/src/lib.rs
@@ -9,7 +9,6 @@ use nakamoto_chain::block::{tree::BlockTree, Height, Time};
 use std::collections::{HashMap, HashSet};
 use std::net;
 use std::sync::{mpsc, Arc, RwLock};
-use std::time::{self, SystemTime, UNIX_EPOCH};
 
 use log::*;
 
@@ -18,7 +17,7 @@ pub const SYNC_THRESHOLD: Height = 144;
 /// Minimum number of peers to be connected to.
 pub const PEER_CONNECTION_THRESHOLD: usize = 3;
 /// Maximum time adjustment between network and local time (70 minutes).
-pub const MAX_TIME_ADJUSTMENT: time::Duration = time::Duration::from_secs(70 * 60);
+pub const MAX_TIME_ADJUSTMENT: peer::TimeOffset = 70 * 60;
 
 /// Identifies a peer.
 pub type PeerId = net::SocketAddr;
@@ -121,28 +120,26 @@ impl<T: BlockTree, P: Peer> Network<T, P> {
     /// the local time plus the median offset of all connected peers.
     ///
     /// Nb. Network time is never adjusted more than 70 minutes from local system time.
-    pub fn network_adjusted_time(&self) -> Time {
-        let local_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    pub fn network_adjusted_time(&self, local_time: Time) -> Time {
         let peers = self.peers.read().expect("lock is not poisoned");
-
         let mut offsets = peers.values().map(Peer::time_offset).collect::<Vec<_>>();
         let count = offsets.len();
 
         offsets.sort();
 
-        let median_offset = if count % 2 == 0 {
+        let median_offset: peer::TimeOffset = if count % 2 == 0 {
             (offsets[count / 2 - 1] + offsets[count / 2]) / 2
         } else {
             offsets[count / 2]
         };
 
-        let adjusted_time = if median_offset > 0 {
-            local_time + time::Duration::from_secs(median_offset as u64).min(MAX_TIME_ADJUSTMENT)
-        } else {
-            local_time - time::Duration::from_secs(-median_offset as u64).min(MAX_TIME_ADJUSTMENT)
-        };
+        let adjustment = median_offset.abs().min(MAX_TIME_ADJUSTMENT) as Time;
 
-        adjusted_time.as_secs() as Time
+        if median_offset > 0 {
+            local_time + adjustment
+        } else {
+            local_time - adjustment
+        }
     }
 
     pub fn listen(&mut self, rx: mpsc::Receiver<peer::Event>) -> Result<(), error::Error> {
@@ -189,5 +186,120 @@ impl<T: BlockTree, P: Peer> Network<T, P> {
             }
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    use super::*;
+
+    use bitcoin::blockdata::constants;
+    use nakamoto_chain::block::cache::model;
+
+    struct TestPeer {
+        time_offset: peer::TimeOffset,
+    }
+
+    impl Peer for TestPeer {
+        fn run<T: BlockTree>(
+            &mut self,
+            _cache: Arc<RwLock<T>>,
+            _events: mpsc::Sender<peer::Event>,
+        ) -> Result<(), error::Error> {
+            Ok(())
+        }
+
+        fn height(&self) -> Height {
+            0
+        }
+
+        fn time_offset(&self) -> peer::TimeOffset {
+            self.time_offset
+        }
+    }
+
+    #[test]
+    fn test_network_adjusted_time() {
+        let network = bitcoin::Network::Bitcoin;
+        let genesis = constants::genesis_block(network).header;
+        let cache = model::Cache::new(genesis);
+        let net: Network<_, TestPeer> =
+            Network::new(Default::default(), Arc::new(RwLock::new(cache)));
+        let local_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as Time;
+
+        {
+            let mut peers = net.peers.write().unwrap();
+            peers.insert(([127, 0, 0, 1], 8333).into(), TestPeer { time_offset: 92 });
+        }
+        assert_eq!(net.network_adjusted_time(local_time), local_time + 92);
+
+        {
+            let mut peers = net.peers.write().unwrap();
+            peers.insert(([127, 0, 0, 2], 8333).into(), TestPeer { time_offset: -32 });
+        }
+        assert_eq!(net.network_adjusted_time(local_time), local_time + 30);
+
+        {
+            let mut peers = net.peers.write().unwrap();
+            peers.insert(([127, 0, 0, 3], 8333).into(), TestPeer { time_offset: 66 });
+        }
+        assert_eq!(net.network_adjusted_time(local_time), local_time + 66);
+
+        {
+            let mut peers = net.peers.write().unwrap();
+            peers.insert(([127, 0, 0, 4], 8333).into(), TestPeer { time_offset: -78 });
+        }
+        assert_eq!(net.network_adjusted_time(local_time), local_time + 17);
+
+        {
+            let mut peers = net.peers.write().unwrap();
+            peers.insert(([127, 0, 0, 5], 8333).into(), TestPeer { time_offset: -1 });
+        }
+        assert_eq!(net.network_adjusted_time(local_time), local_time - 1);
+
+        {
+            let mut peers = net.peers.write().unwrap();
+            [
+                (([127, 0, 0, 6], 8333), 43818),
+                (([127, 0, 0, 7], 8333), 91234),
+                (([127, 0, 0, 8], 8333), 38183),
+                (([127, 0, 0, 9], 8333), 8961),
+                (([127, 0, 0, 0], 8333), 9131),
+            ]
+            .iter()
+            .cloned()
+            .for_each(|(addr, time_offset)| {
+                peers.insert(addr.into(), TestPeer { time_offset });
+            });
+        }
+        assert_eq!(
+            net.network_adjusted_time(local_time),
+            local_time + MAX_TIME_ADJUSTMENT as Time
+        );
+
+        {
+            let mut peers = net.peers.write().unwrap();
+            [
+                (([127, 0, 0, 6], 8333), -43818),
+                (([127, 0, 0, 7], 8333), -91234),
+                (([127, 0, 0, 8], 8333), -38183),
+                (([127, 0, 0, 9], 8333), -8961),
+                (([127, 0, 0, 0], 8333), -9131),
+            ]
+            .iter()
+            .cloned()
+            .for_each(|(addr, time_offset)| {
+                peers.insert(addr.into(), TestPeer { time_offset });
+            });
+        }
+        assert_eq!(
+            net.network_adjusted_time(local_time),
+            local_time - MAX_TIME_ADJUSTMENT as Time
+        );
     }
 }
