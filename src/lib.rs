02925d2abfaffb2971ac463449262076bc0f4ad5
Alexis Sellier <self@cloudhead.io>
2020-07-29 08:58:08 +0200

    Create top-level "nakamoto" crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
-pub mod logger;
+#[cfg(feature = "nakamoto-chain")]
+pub use nakamoto_chain as chain;
+#[cfg(feature = "nakamoto-daemon")]
+pub use nakamoto_daemon as daemon;
+#[cfg(feature = "nakamoto-node")]
+pub use nakamoto_node as node;
+#[cfg(feature = "nakamoto-p2p")]
+pub use nakamoto_p2p as p2p;
+
+#[cfg(test)]
+#[cfg(feature = "nakamoto-test")]
+pub use nakamoto_test as test;
